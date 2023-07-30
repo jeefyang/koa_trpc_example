bf5dd2d1ee5a827488ebde35011818429ad9bc0e
jeefyang <jeefyjl@gmail.com>
2023-07-30 15:17:28 +0800

    去掉多余代码

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 
-import { initTRPC } from "@trpc/server"
 import koa from "koa";
 import { koaBody } from "koa-body"
 import koaRouter from "koa-router";
 import { appRouter } from './router';
-import { AnyRouter } from '@trpc/server';
 // import { createKoaMiddleware } from "./koaMiddle"
 import { createKoaMiddleware } from "trpc-koa-adapter"
 
@@ -13,19 +11,13 @@ async function main() {
     // express implementation
     const app: koa = new koa()
     const kRouter = new koaRouter()
-    const trpc = initTRPC.create()
 
     kRouter.get("/", async (ctx) => {
-        // return 'Server is running!'
         ctx.type = "html"
         ctx.body = 'Server is running!'
         return ctx
     })
     const adapter = createKoaMiddleware({ router: appRouter, prefix: "/trpc" })
-    // kRouter.get('/trpc', createExpressMiddleware({
-    //     router: appRouter,
-    //     createContext: () => ({}),
-    // }))
     app.use(koaBody({}))
     app.use(kRouter.routes())
     app.use(adapter)
